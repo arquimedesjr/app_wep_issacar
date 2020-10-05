c1041be8530b95058fe3f40fdd6d45f76f82f841
Arquimedes <arquimedeslqjunior@gmail.com>
2020-10-04 22:23:48 -0300

    Ajuste nas imagens

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-
 # class AccountAdmin(UserAdmin):
 #     list_display = (
 #         'email', 'username', 'date_joined', 'is_admin', 'is_active',
