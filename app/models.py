37e676f603eeeeb88fe75e43e42af32d8c1749e1
Arquimedes <arquimedeslqjunior@gmail.com>
2020-10-04 22:27:35 -0300

    Ajuste nas imagens

--- app/models.py
+++ app/models.py
@@ -46,7 +46,7 @@ class Grupo(models.Model):
 
 class Jovens(models.Model):
     foto = models.ImageField(upload_to='profile_pics', verbose_name='Foto')
-    foto_base64 = models.CharField(max_length=2000000000000000)
+    foto_base64 = models.CharField(max_length=99999999999)
     nome = models.CharField(max_length=250, null=False, verbose_name='Nome - Ex: (Arquimedes Junior)')
     telefone = models.CharField(max_length=11, null=False, verbose_name='Telefone - Ex: 11948924982')
     tribo = models.ForeignKey(Tribo, on_delete=models.CASCADE)
